5bd3f044430fd1e628c91cab80831dd3a6e76f7a
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-07-22 22:37:57 -0600

    Agregué los métodos para las funciones de edit y update

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
 
 class TaskController extends Controller
@@ -11,7 +12,7 @@ class TaskController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
         $tasks = Task::latest()->paginate(3);
         return view('index', ['tasks' => $tasks]);
@@ -20,7 +21,7 @@ class TaskController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
         return view('create');
     }
@@ -50,17 +51,23 @@ class TaskController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Task $task)
+    public function edit(Task $task): View
     {
-        //
+        return view('edit', ['task' => $task]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Task $task)
+    public function update(Request $request, Task $task): RedirectResponse
     {
-        //
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+
+        $task->update($request->all());
+        return redirect()->route('tasks.index')->with('success', 'Nueva tarea actualizada exitosamente');
     }
 
     /**
